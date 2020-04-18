100f29c91ee9f4653f4c9f83c41a20064a07166b
jih200 <jiafeng_han@hotmail.com>
2020-04-18 23:34:42 +0800

    add shader creation/compilation/linking

--- CREATE_A_WINDOW/main.cpp
+++ CREATE_A_WINDOW/main.cpp
@@ -1,15 +1,27 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
 
+const char* readFileIntoString(const char*);
 void framebuffer_size_callback(GLFWwindow*, int, int);
 void processInput(GLFWwindow*);
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+const char* vertexShaderSource = "#version 330 core\n layout (location = 0) in vec3 aPos;void main(){gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}";
+const char* fragmentShaderSource = "#version 330 core\n out vec4 FragColor;void main(){FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}";
 
 
 int main()
 {
 	glfwInit();
+
 	// check the GLFW version, 3.3 and core profile
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -36,6 +48,54 @@ int main()
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// generate a buffer ID for VBO: vertex buffer objects
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// compile a veretx shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// check shader compiling
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+			<< infoLog << std::endl;
+	}
+
+	//compile a fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// create a shader program and link the shaders to it
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n"
+			<< infoLog << std::endl;
+	}
+	glUseProgram(shaderProgram);
+	// the deletion can be done after the link
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
 	// rendering loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -56,6 +116,18 @@ int main()
 	glfwTerminate();
 
 	return 0;
+}
+
+// try to use file reading to replace the direct assignment
+const char* readFileIntoString(const char* filename)
+{
+	std::ifstream ifile(filename);
+	std::ostringstream buf;
+	char ch;
+	
+	while (buf&&ifile.get(ch))
+		buf.put(ch);
+	return const_cast<const char*>(buf.str().c_str());
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
