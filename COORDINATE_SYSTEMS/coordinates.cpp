e36211065f37c25200acace6a26ac4eb872a432a
jih200 <jiafeng_han@hotmail.com>
2020-05-08 22:26:39 +0800

    finish the Coordinate systems project

--- COORDINATE_SYSTEMS/coordinates.cpp
+++ COORDINATE_SYSTEMS/coordinates.cpp
@@ -5,6 +5,8 @@
 #define WINDOW_HEIGHT 600
 #define WINDOW_TITLE "COORDINATE SYSTEMS"
 
+#define DIM_MODE 3
+
 typedef glm::vec3 VEC3;
 typedef glm::mat4 MAT4;
 
@@ -41,9 +43,10 @@ int main()
 	}
 
 	// get an instance object of shader
+	glEnable(GL_DEPTH_TEST);
 	Shader shader(VERTEX_SHADER_PATH, FRAGMENT_SHADER_PATH);
 
-
+#if DIM_MODE == 2
 	// define the vertices and indices
 	float vertices[] = {
 		// positions        // texture coords
@@ -74,6 +77,78 @@ int main()
 	// get texture coord information s t (r)
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+#elif DIM_MODE == 3
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	// using different positions to create multiple identical cubes
+	VEC3 cubePositions[] = {
+		VEC3(0.0f,  0.0f,   0.0f),
+		VEC3(2.0f,  5.0f, -15.0f),
+		VEC3(-1.5f, -2.2f,  -2.5f),
+		VEC3(-3.8f, -2.0f, -12.3f),
+		VEC3(2.4f, -0.4f,  -3.5f),
+		VEC3(-1.7f,  3.0f,  -7.5f),
+		VEC3(1.3f, -2.0f,  -2.5f),
+		VEC3(1.5f,  2.0f,  -2.5f),
+		VEC3(1.5f,  0.2f,  -1.5f),
+		VEC3(-1.3f,  1.0f,  -1.5f),
+	};
+	
+	unsigned int VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+#endif
 
 	// create variables and set up for image loading
 	stbi_set_flip_vertically_on_load(true);
@@ -128,24 +203,10 @@ int main()
 	}
 	stbi_image_free(data);
 
-	// setting coords transform matrices
-	// don't forget to initialize the MAT4 matrices with a Identity Matrix
-	MAT4 model(1.0f);
-	model = glm::rotate(model, glm::radians(-55.0f), VEC3(1.0f, 0.0f, 0.0f));
-
-	MAT4 view(1.0f);
-	view = glm::translate(view, VEC3(0.0f, 0.0f, -3.0f));
-
-	MAT4 projection(1.0f);
-	projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f);
-
 	shader.use();
 	shader.setInt("ourTexture0", 0);
 	shader.setInt("ourTexture1", 1);
 	shader.setFloat("ratio", 0.2f);
-	shader.setMat4("model", model);
-	shader.setMat4("view", view);
-	shader.setMat4("projection", projection);
 
 	// how to dynamically get viewport width and height ?
 	// create a orthographic projection matrix
@@ -156,7 +217,7 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		processInput(window);
 
@@ -166,19 +227,62 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 
+#if DIM_MODE == 2
+		// setting coords transform matrices
+		// don't forget to initialize the MAT4 matrices with a Identity Matrix
+		MAT4 model(1.0f);
+		model = glm::rotate(model, glm::radians(-55.0f), VEC3(1.0f, 0.0f, 0.0f));
+
+		MAT4 view(1.0f);
+		view = glm::translate(view, VEC3(0.0f, 0.0f, -3.0f));
+
+		MAT4 projection(1.0f);
+		projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f);
+		
+		shader.setMat4("model", model);
+		shader.setMat4("view", view);
+		shader.setMat4("projection", projection);
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+#elif DIM_MODE == 3
+		glBindVertexArray(VAO);
+		for (int i = 0; i < 10; ++i)
+		{
+			MAT4 model(1.0f), view(1.0f), projection(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			if (i % 3 == 0)
+			{
+				model = glm::rotate(model, glm::radians(20.0f * i), VEC3(1.0f, 0.3f, 0.5f));
+			}
+			else
+			{
+				model = glm::rotate(model, (float)glfwGetTime() * glm::radians(20.0f * i), VEC3(1.0f, 0.3f, 0.5f));
+			}
+			view = glm::translate(view, VEC3(0.0f, 0.0f, -3.0f));
+			// view = glm::translate(view, VEC3(sin((float)glfwGetTime()) * 3, 0.0f, cos((float)glfwGetTime()) * 5));
+			projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f);
 
+
+			shader.setMat4("model", model);
+			shader.setMat4("view", view);
+			shader.setMat4("projection", projection);
+			
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+#endif
+		glBindTexture(GL_TEXTURE_2D, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	glBindTexture(GL_TEXTURE_2D, 0);
 	glDeleteBuffers(1, &VBO);
+#if DIM_MODE == 2
 	glDeleteBuffers(1, &EBO);
+#endif
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteTextures(1, &texture0);
 	glDeleteTextures(1, &texture1);
 
+	glDisable(GL_DEPTH_TEST);
 	glfwTerminate();
 	return 0;
 }
