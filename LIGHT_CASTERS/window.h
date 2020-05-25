b3436143431193e858df93a9e52207c1a257b1ce
hanjiafeng <hanjiafeng2398@virtuos.com.cn>
2020-05-25 11:58:44 +0800

    add project LIGHT_CASTERS

--- LIGHT_CASTERS/window.h
+++ LIGHT_CASTERS/window.h
@@ -7,7 +7,7 @@ namespace Window
 {
 	constexpr auto WIDTH  = 800;
 	constexpr auto HEIGHT = 600;
-	constexpr auto TITLE  = "LIGHT MAPS";
+	constexpr auto TITLE  = "LIGHT CASTERS";
 
 	constexpr auto COLOR = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
 }
